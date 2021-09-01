137c5445bfe5277831c18b247f4675db913eb332
ka1peshc <kalpesh111997@gmail.com>
2021-09-01 11:07:39 +0530

    Add wage calulation function

--- EmpWageProblem/Program.cs
+++ EmpWageProblem/Program.cs
@@ -8,19 +8,28 @@ namespace EmpWageProblem
         public const int IS_PRESENT = 0;
         public const int IS_ABSENT = 1;
         public const int IS_PART_TIME = 2;
+        public const int WAGE_PER_HOUR = 20;
+        public const int FULL_DAY_HOUR = 8;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
+            int empHour = 0;
+            int salary = 0;
             Random random = new Random();
             int empCheck = random.Next(0,2);
+            
             if (empCheck == IS_PRESENT)
             {
-                Console.WriteLine("Employee is present");
+                empHour = FULL_DAY_HOUR;
+                Console.WriteLine("Employee is Present");
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                Console.WriteLine("Employee is Absent");
+                empHour = 0;
             }
+            salary = empHour * WAGE_PER_HOUR;
+            Console.WriteLine(salary);
         }
     }
 }
