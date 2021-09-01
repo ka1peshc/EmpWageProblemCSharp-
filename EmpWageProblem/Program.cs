e65a68abd3741eeb3ce8477b9b7c6be6470c1e9e
ka1peshc <kalpesh111997@gmail.com>
2021-09-01 10:53:07 +0530

    add employee attedence functionality

--- EmpWageProblem/Program.cs
+++ EmpWageProblem/Program.cs
@@ -5,10 +5,22 @@ namespace EmpWageProblem
 {
     public class Program
     {
+        public const int IS_PRESENT = 0;
+        public const int IS_ABSENT = 1;
+        public const int IS_PART_TIME = 2;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
-            
+            Random random = new Random();
+            int empCheck = random.Next(0,2);
+            if (empCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
