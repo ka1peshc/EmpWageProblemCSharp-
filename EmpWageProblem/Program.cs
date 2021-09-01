047e798a64443848e34cc21ff7491b80dce0f6b8
ka1peshc <kalpesh111997@gmail.com>
2021-09-01 10:28:51 +0530

    First commit, Display Welcome message

---
+++ EmpWageProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace EmpWageProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+            
+        }
+    }
+}
