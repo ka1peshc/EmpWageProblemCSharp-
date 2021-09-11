bb8e88d2575135776879d92a0b317baaa56ede1e
ka1peshc <kalpesh111997@gmail.com>
2021-09-11 08:46:59 +0530

    Created new file for calculating Emp wage and call method from main method

---
+++ EmpWageProblem/EmpWageCalculator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWageProblem
+{
+    public class EmpWageCalculator
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        //class variables
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonths;
+        private int salary = 0;
+        public EmpWageCalculator(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonths)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonths = maxHoursPerMonths;
+        }
+        public void computeEmpWage()
+        {
+            int empHour = 0, workingDay = 0, totalHours = 0;
+            int empCheck;
+            Random random = new Random();
+            while (workingDay <= numOfWorkingDays && totalHours <= maxHoursPerMonths)
+            {
+                workingDay++;
+                empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHour = 8;
+                        break;
+                    case IS_FULL_TIME:
+                        empHour = 4;
+                        break;
+                    default:
+                        empHour = 0;
+                        break;
+                }
+                totalHours += empHour;
+                Console.WriteLine("Day's {0} Emp hours {1}", workingDay, totalHours);
+            }
+            salary = totalHours * empRatePerHour;
+            Console.WriteLine("total Emp wage for company: {0} is {1}", company, salary);
+        }
+
+        public string displaySalary()
+        {
+            return "total Emp wage for company: " + company + " is " + salary;
+        }
+    }
+}
